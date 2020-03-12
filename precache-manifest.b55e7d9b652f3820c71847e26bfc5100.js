bf74c38d4077646d657fe095fe27a6940fd208ca
Nabin7721 <57736746+Nabin7721@users.noreply.github.com>
2020-03-11 20:09:45 -0400

    Updates

---
+++ precache-manifest.b55e7d9b652f3820c71847e26bfc5100.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "212d5b28c4fba43809564cff5043897d",
+    "url": "/Church-website/index.html"
+  },
+  {
+    "revision": "9ee6742d902fc3cc5b43",
+    "url": "/Church-website/static/css/main.05511dc0.chunk.css"
+  },
+  {
+    "revision": "d81678fb3c0b7079ab8a",
+    "url": "/Church-website/static/js/2.8fad6bb6.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Church-website/static/js/2.8fad6bb6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9ee6742d902fc3cc5b43",
+    "url": "/Church-website/static/js/main.0d3d9661.chunk.js"
+  },
+  {
+    "revision": "e292247bc2e59e8a93c8",
+    "url": "/Church-website/static/js/runtime-main.e852464c.js"
+  },
+  {
+    "revision": "5c5671e6da9030e8336914453cc210a0",
+    "url": "/Church-website/static/media/about-abst.5c5671e6.jpg"
+  }
+]);
